9ef2fd8339cc03163abfb993abe09073bd6e6956
FErnando Schettini <FernandoSchettini@outlook.com>
2024-08-12 12:38:52 -0300

    [FEAT]: script for running expriment

---
+++ run_experiment.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#   Author: Fernando Schettini 
+#   Description: This script runs Paxos Algorithm with a given number of nodes.
+#   How to run: ./run_experiment.sh <num_nodes>
+
+num_nodes=$1
+start_port=5000
+
+proposer_add=()
+acceptor_add=()
+learner_add=()
+
+# Generate addresses for each node
+for i in $(seq 0 $(($num_nodes-1)))
+do
+    proposer_port=$(($start_port + $i))
+    acceptor_port=$(($proposer_port + $num_nodes))
+    learner_port=$(($acceptor_port + $num_nodes))
+
+    proposer_add+=("127.0.0.1:${proposer_port}")
+    acceptor_add+=("127.0.0.1:${acceptor_port}")
+    learner_add+=("127.0.0.1:${learner_port}")
+done
+
+all_acceptor_addresses=$(IFS=, ; echo "${acceptor_add[*]}")
+
+# Start nodes
+for i in $(seq 0 $(($num_nodes-1)))
+do
+    nohup python3 main.py -a proposer -os ${proposer_add[$i]} -s ${acceptor_addresses} > proposer_${i}.log 2>&1 &
+    nohup python3 main.py -a acceptor -os ${acceptor_add[$i]} > acceptor_${i}.log 2>&1 &
+    nohup python3 main.py -a learner -os ${learner_add[$i]} > learner_${i}.log 2>&1 &
+
+done
+
+echo "Proposers: ${proposer_add[@]}"
+echo "Acceptors: ${acceptor_add[@]}"
+echo "Learners: ${learner_add[@]}"
